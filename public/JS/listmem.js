ae4fb0a84a0c4be3b5916720f58a468ed38d8436
Shardul <iamsharduld@gmail.com>
2018-06-15 02:57:55 +0530

    added back button in list members

--- public/JS/listmem.js
+++ public/JS/listmem.js
@@ -7,10 +7,10 @@ function SmartSociety(){
 	MemberRef.on("child_added", function(data, prevChildKey) {
 	   var newMember = data.val();
 	   console.log("name: " + newMember.Name);
-	   console.log("age: " + newMember.Email);
+	   console.log("Email: " + newMember.Email);
 	   g = document.createElement('addHere');
 		g.setAttribute("class", "list-group-item");
-		var textnode = document.createTextNode(newMember.Email);         // Create a text node
+		var textnode = document.createTextNode(newMember.Name+", Flat no. "+newMember.Address);         // Create a text node
 		g.appendChild(textnode);   
 		document.getElementById("myList").appendChild(g);
 	
