7a274b7e1996d3740a6f00eba341f8411b78ed27
Shardul <iamsharduld@gmail.com>
2018-06-15 02:35:54 +0530

    lists all members

---
+++ public/JS/listmem.js
@@ -0,0 +1,26 @@
+var MemberRef = firebase.database().ref("/Members")
+//console.log("hello")
+
+// Event listener for Add Member button
+function SmartSociety(){
+
+	MemberRef.on("child_added", function(data, prevChildKey) {
+	   var newMember = data.val();
+	   console.log("name: " + newMember.Name);
+	   console.log("age: " + newMember.Email);
+	   g = document.createElement('addHere');
+		g.setAttribute("class", "list-group-item");
+		var textnode = document.createTextNode(newMember.Email);         // Create a text node
+		g.appendChild(textnode);   
+		document.getElementById("myList").appendChild(g);
+	
+	});
+
+	
+};
+
+
+
+window.onload = function(){
+	window.SmartSociety = new SmartSociety();
+};
